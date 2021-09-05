85cbf2abc606620cb0de6e7bda3a2feee7df4e69
Dwitesh Shetty <dwitesh.shetty@gmail.com>
2021-09-06 01:46:31 +0530

    Home typo, hover menu css

--- src/components/Header/HoverMenu.tsx
+++ src/components/Header/HoverMenu.tsx
@@ -6,17 +6,17 @@ function Menu(): JSX.Element {
   return (
     <ul className={styles.navLinks}>
       <li>
-        <NavLink to="/about" exact activeClassName="selected">
+        <NavLink to="/about" exact activeClassName={styles.selected}>
           About
         </NavLink>
       </li>
       <li>
-        <NavLink to="/work" exact activeClassName="selected">
+        <NavLink to="/work" exact activeClassName={styles.selected}>
           Work
         </NavLink>
       </li>
       <li>
-        <NavLink to="/contact" exact activeClassName="selected">
+        <NavLink to="/contact" exact activeClassName={styles.selected}>
           Contact
         </NavLink>
       </li>
