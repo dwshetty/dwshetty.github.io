29c4bb9e53bb7c45e40d0b64b938f3bf83b771c7
dwshetty <dwshetty@users.noreply.github.com>
2021-09-07 19:11:07 +0000

    deploy: 142c46f1088a6ed4d6f6bd08ab1b6866a5143ac2

--- static/js/ScrollToTop.3f2daac5.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpdwshetty=this.webpackJsonpdwshetty||[]).push([[5],{44:function(t,e,n){"use strict";n.r(e);var s=n(1),c=n(2);e.default=function(){var t=Object(c.f)().pathname;return Object(s.useEffect)((function(){window.scrollTo(0,0)}),[t]),null}}}]);
-//# sourceMappingURL=ScrollToTop.3f2daac5.chunk.js.map
