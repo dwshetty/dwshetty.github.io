22f99861fb7c90991df4e57d4b29d893b0f0e7f1
Dwitesh Shetty <dwitesh.shetty@gmail.com>
2022-08-23 23:27:10 +0530

    trying environment again

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const ScrollToTop = lazy(
 
 function App(): JSX.Element {
   // eslint-disable-next-line no-console
-  console.log("REACT_APP_HOST", process.env.REACT_APP_HOST_ENDPOINT);
+  console.log("REACT_APP_HOST_ENDPOINT", process.env.REACT_APP_HOST_ENDPOINT);
   return (
     <BrowserRouter>
       <Suspense fallback={<></>}>
