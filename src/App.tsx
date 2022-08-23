6f13c043ded19d8be61e8abb8ba7f625ae1fc916
Dwitesh Shetty <dwitesh.shetty@gmail.com>
2022-08-24 00:11:46 +0530

    env variables mentioned individually

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,6 @@ const ScrollToTop = lazy(
 );
 
 function App(): JSX.Element {
-  // eslint-disable-next-line no-console
-  console.log("REACT_APP_HOST_ENDPOINT", process.env.REACT_APP_HOST_ENDPOINT);
   return (
     <BrowserRouter>
       <Suspense fallback={<></>}>
