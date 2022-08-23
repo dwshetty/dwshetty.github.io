51e5a3fda735e0ff11e1b20d6a5c9631bb71bfb1
Dwitesh Shetty <dwitesh.shetty@gmail.com>
2022-08-23 22:54:24 +0530

    adding secret to gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ const ScrollToTop = lazy(
 );
 
 function App(): JSX.Element {
+  console.log("REACT_APP_HOST_ENDPOINT", process.env.REACT_APP_HOST_ENDPOINT);
   return (
     <BrowserRouter>
       <Suspense fallback={<></>}>
