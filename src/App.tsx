e570e3cc19a78c922152cc654b146f83a4017a11
Dwitesh Shetty <dwitesh.shetty@gmail.com>
2022-08-24 00:36:23 +0530

    console log added

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ const ScrollToTop = lazy(
 );
 
 function App(): JSX.Element {
+  // eslint-disable-next-line no-console
+  console.log("REACT_APP_HOST_URL", process.env.REACT_APP_HOST_URL);
   return (
     <BrowserRouter>
       <Suspense fallback={<></>}>
